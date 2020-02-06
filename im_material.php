227fc7156c8232053c0299f0ab659e4503f98afe
VCK <hf520777888@163.com>
2020-02-06 16:39:00 +0800

    VCK
    VCK

---
+++ im_material.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by yaaaaa__god 
+ * User: GOD
+ * Date: 2018-12-29 10:02:26
+ * Desc: IM素材
+ */
+namespace app\model;
+
+
+class im_material extends BaseModel
+{
+    public $title = 'im_material';
+
+    
+}
---
+++ im_text.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by yaaaaa__god 
+ * User: GOD
+ * Date: 2018-12-29 10:02:26
+ * Desc: IM自动回复
+ */
+namespace app\model;
+
+
+class im_text extends BaseModel
+{
+    public $title = 'im_text';
+
+    
+}
